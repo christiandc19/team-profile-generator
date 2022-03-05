143117f0ed4172f008ff0402c1b47b4e306d705f
Chris <christiandc19@Chriss-MBP.lan>
2022-03-05 15:40:01 -0800

    added icons

--- generateHTML.js
+++ generateHTML.js
@@ -3,12 +3,12 @@ const generateManager = function (Manager) {
     <div id="card">
     <div class="card-body bg-primary">
       <h5 class="card-title">${Manager.name}</h5>
-      <p class="card-text">Manager</p>
+      <p class="card-text"> <i class="fa fa-solid fa-mug-hot"></i> Manager</p>
     </div>
     <ul class="list-group list-group-flush">
       <li class="list-group-item">ID Number: ${Manager.id}</li>
-      <li class="list-group-item"><a href="mailto:${Manager.email}">Email: ${Manager.email}</a></li>
-      <li class="list-group-item"><p class="card-text mt-2">Office Number: ${Manager.office}</p></li>
+      <li class="list-group-item">Email: <a href="mailto:${Manager.email}">${Manager.email}</a></li>
+      <li class="list-group-item"><p class="card-text mt-2">Office Number: ${Manager.officeNumber}</p></li>
     </ul>
     </div>
     `;
@@ -19,12 +19,12 @@ const generateEngineer = function (Engineer) {
     <div id="card">
     <div class="card-body bg-primary">
       <h5 class="card-title">${Engineer.name}</h5>
-      <p class="card-text">Engineer</p>
+      <p class="card-text"> <i class="fa fa-solid fa-glasses"></i> Engineer</p>
     </div>
     <ul class="list-group list-group-flush">
       <li class="list-group-item">ID Number: ${Engineer.id}</li>
-      <li class="list-group-item"><a href="mailto:${Engineer.email}" class="card-link">${Engineer.email}</a></li>
-      <li class="list-group-item"><p class="card-text mt-2"><a href="https://github.com/${Engineer.github}">Github: ${Engineer.github}</a></p></li>
+      <li class="list-group-item">Email: <a href="mailto:${Engineer.email}" class="card-link">${Engineer.email}</a></li>
+      <li class="list-group-item"><p class="card-text mt-2">Github: <a href="https://github.com/${Engineer.github}">${Engineer.github}</a></p></li>
     </ul>
     </div>
     `;
@@ -35,13 +35,12 @@ const generateIntern = function (Intern) {
     <div id="card">
     <div class="card-body bg-primary">
       <h5 class="card-title">${Intern.name}</h5>
-      <p class="card-text">Intern</p>
+      <p class="card-text"> <i class="fa fa-solid fa-user-graduate"></i> Intern</p>
     </div>
     <ul class="list-group list-group-flush">
       <li class="list-group-item">ID Number: ${Intern.id}</li>
-      <li class="list-group-item"><a href="mailto:${Intern.email}">Email: ${Intern.email}</a></li>
-      <li class="list-group-item"><p ${Intern.school} >${Intern.school}</p>
-      </li>
+      <li class="list-group-item">Email: <a href="mailto:${Intern.email}"> ${Intern.email}</a></li>
+      <li class="list-group-item">School: ${Intern.school}</li>
     </ul>
     </div>
     `;
@@ -74,6 +73,7 @@ generateHTML = (myTeam) => {
 
 
     const cards = arr.join("");
+    
     console.log(cards);
 
     const generateTeam = generateTeamCard(cards);
@@ -89,6 +89,9 @@ return `
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <!-- Font Awesome Icon -->
+    <script src="https://kit.fontawesome.com/a4ff3ab2fb.js" crossorigin="anonymous"></script>
+
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="./dist/style.css">
@@ -97,13 +100,9 @@ return `
   <body>
       
         <!-- NavBar -->
-
-    <nav class="navbar navbar-light bg-light">
-        <div class="container-fluid">
-          <span class="navbar-brand mb-0 h1">My Team</span>
-        </div>
-      </nav>
-
+<div id="header">
+    <h1>My Team</h1>
+</div>
     <!-- Cards -->
 
 <div class="row mt-5 mb-5" id="cards">
--- index.js
+++ index.js
@@ -16,91 +16,193 @@ return inquirer
         type: "list",
         name: "role",
         message: "Which team member would you like to add? (Required)",
-        choices: ["Engineer", "Manager", "Intern"],
+        choices: ["Engineer", "Manager", "Intern"]
     },
 
     {
         when: (data) => data.role === "Manager" ,
         type: "input",
         name: "name",
-        message: "What is the manager's name? (Required)"
+        message: "What is the manager's name? (Required)",
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log("Please Enter a name.");
+                return false;
+            }
+        }
     },
 
     {
         when: (data) => data.role === "Manager",
         type: "input",
         name: "id",
-        message: "What is the manager's id? (Required)"
+        message: "What is the manager's id? (Required)",
+        validate: idInput => {
+            if (idInput) {
+                return true;
+            } else {
+                console.log("Please Enter manage's id number.");
+                return false;
+            }
+        }
+
     },
 
     {
         when: (data) => data.role === "Manager",
         type: "input",
         name: "email",
-        message: "What is the manager's email? (Required)"
+        message: "What is the manager's email? (Required)",
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else {
+                console.log("Please enter manager's email.");
+                return false;
+            }
+        }
+
     },
 
     {
         when: (data) => data.role === "Manager", 
         type: "input",
         name: "office",
-        message: "What is the manager's office number? (Required)"
+        message: "What is the manager's office number? (Required)",
+        validate: officeInput => {
+            if (officeInput) {
+                return true;
+            } else {
+                console.log("Please Enter manager's office number.");
+                return false;
+            }
+        }
     },
 
     {
         when: (data) => data.role === "Engineer",
         type: "input",
         name: "name",
-        message: "What is the engineer's name? (Required)"
+        message: "What is the engineer's name? (Required)",
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log("Please enter engineer's name.");
+                return false;
+            }
+        }
     },
 
     {
         when: (data) => data.role === "Engineer", 
         type: "input",
         name: "id",
-        message: "What is the engineer's id? (Required)"
+        message: "What is the engineer's id? (Required)",
+        validate: idInput => {
+            if (idInput) {
+                return true;
+            } else {
+                console.log("Please enter Engineer's id number.");
+                return false;
+            }
+        }
     },
 
     {
         when: (data) => data.role === "Engineer",
         type: "input",
         name: "email",
-        message: "What is the engineer's email? (Required)"
+        message: "What is the engineer's email? (Required)",
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else {
+                console.log("Please enter engineer's email.");
+                return false;
+            }
+        }
     },
 
     {
         when: (data) => data.role === "Engineer",
         type: "input",
         name: "github",
-        message: "What is the engineer's Github Username? (Required)"
+        message: "What is the engineer's Github Username? (Required)",
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log("Please enter engineer's github username.");
+                return false;
+            }
+        }
+
     },
 
     {
         when: (data) => data.role === "Intern",
         type: "input",
         name: "name",
-        message: "What is the Intern's name? (Required)"
+        message: "What is the Intern's name? (Required)",
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log("Please enter intern's name.");
+                return false;
+            }
+        }
+
     },
 
     {
         when: (data) => data.role === "Intern",
         type: "input",
         name: "id",
-        message: "What is the intern's id? (Required)"
+        message: "What is the intern's id? (Required)",
+        validate: idInput => {
+            if (idInput) {
+                return true;
+            } else {
+                console.log("Please enter intern's id number.");
+                return false;
+            }
+        }
+
     },
 
     {
         when: (data) => data.role === "Intern",
         type: "input",
         name: "email",
-        message: "What is the Intern's email? (Required)"
+        message: "What is the Intern's email? (Required)",
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else {
+                console.log("Please enter intern's email address.");
+                return false;
+            }
+        }
     },
 
     {
         when: (data) => data.role === "Intern",
         type: "input",
         name: "school",
-        message: "What is the name of the Intern's school? (Required)"
+        message: "What is the name of the Intern's school? (Required)",
+        validate: schoolInput => {
+            if (schoolInput) {
+                return true;
+            } else {
+                console.log("Please enter intern's school.");
+                return false;
+            }
+        }
+
     },
 
     {
@@ -120,7 +222,7 @@ return inquirer
             myTeam.push(engineer);
         }
         if (data.role === "Intern") {
-            const intern = new Intern(data.name, data.id, data.email, data.office);
+            const intern = new Intern(data.name, data.id, data.email, data.school);
             myTeam.push(intern);
         }
         if (data.newMember === "Yes") {
